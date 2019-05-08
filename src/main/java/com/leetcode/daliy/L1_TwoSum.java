2746ce4bde4385887df33da9572d109eca58d186
tsn <tsn@babelbank.io>
2019-05-08 22:36:35 +0800

    :hankey: Writing bad code that needs to be improved.

--- src/main/java/com/leetcode/daliy/L1_TwoSum.java
+++ src/main/java/com/leetcode/daliy/L1_TwoSum.java
@@ -28,6 +28,7 @@ public class L1_TwoSum {
     public static void main(String[] args) {
         int num[] = new int[]{2,7,11,15};
         System.out.println(Arrays.toString(twoSum(num,9)));
+        System.out.println("hi");
 
     }
 
