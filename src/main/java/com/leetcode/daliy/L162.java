b830c40848021783f3d1cd86280f96aa26b68d0f
tsn <tanshaonan@vip.qq.com>
2019-11-12 00:03:19 +0800

    :sparkles: 162

---
+++ src/main/java/com/leetcode/daliy/L162.java
@@ -0,0 +1,22 @@
+package com.leetcode.daliy;
+
+/**
+ * @author TSN
+ * @date 2019/11/11
+ * @Description
+ */
+public class L162 {
+    public static void main(String[] args) {
+        int a[] = new int[]{1,2,1,3,5,6,4};
+        System.out.println(findPeakElement(a));
+    }
+    public static int findPeakElement(int[] nums) {
+
+        for (int i = 0; i < nums.length-1; i++) {
+            if (nums[i+1]<nums[i]){
+                return i;
+            }
+        }
+        return nums.length-1;
+    }
+}
